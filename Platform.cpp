ef1ab23a01467a2e02e66dba2302cadc69d01ced
K4MIN4R <GhostPL1223@o2.pl>
2024-06-13 20:50:49 +0200

    Update Platform.cpp

--- Platform.cpp
+++ Platform.cpp
@@ -1,1 +1,17 @@
 
+//platform.cpp
+#include "Platform.h"
+#include <allegro5/allegro_primitives.h>
+
+const float Platform::width = 80;
+const float Platform::height = 20;
+
+Platform::Platform(float x, float y) : x(x), y(y) {}
+
+void Platform::moveDown(float speed) {
+    y += speed;
+}
+
+void Platform::draw() const {
+    al_draw_filled_rectangle(x - width / 2, y - height / 2, x + width / 2, y + height / 2, al_map_rgb(255, 255, 255));
+}
