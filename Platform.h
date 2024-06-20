813a6f9b6a36bb88576936beb117e1782c87d7f3
K4MIN4R <GhostPL1223@o2.pl>
2024-06-20 22:22:36 +0200

    Update Platform.h

--- Platform.h
+++ Platform.h
@@ -1,22 +1,63 @@
-//platform.h
 #ifndef PLATFORM_H
 #define PLATFORM_H
 
+/**
+ * @brief Klasa reprezentująca platformę.
+ */
 class Platform {
 public:
+    /**
+     * @brief Konstruktor klasy Platform.
+     * 
+     * @param x Pozycja pozioma platformy.
+     * @param y Pozycja pionowa platformy.
+     */
     Platform(float x, float y);
 
-    void moveDown(float speed); // Method to move the platform downwards
+    /**
+     * @brief Przesuwa platformę w dół.
+     * 
+     * @param speed Prędkość poruszania się platformy.
+     */
+    void moveDown(float speed);
+
+    /**
+     * @brief Rysuje platformę na ekranie.
+     */
     void draw() const;
 
+    /**
+     * @brief Zwraca pozycję poziomą platformy.
+     * 
+     * @return float Pozycja pozioma platformy.
+     */
     float getX() const { return x; }
+
+    /**
+     * @brief Zwraca pozycję pionową platformy.
+     * 
+     * @return float Pozycja pionowa platformy.
+     */
     float getY() const { return y; }
+
+    /**
+     * @brief Zwraca szerokość platformy.
+     * 
+     * @return float Szerokość platformy.
+     */
     float getWidth() const { return width; }
+
+    /**
+     * @brief Zwraca wysokość platformy.
+     * 
+     * @return float Wysokość platformy.
+     */
     float getHeight() const { return height; }
 
 private:
-    float x, y;
-    static const float width, height;
+    float x, y; ///< Pozycje platformy
+    static const float width; ///< Szerokość platformy
+    static const float height; ///< Wysokość platformy
 };
 
 #endif
