dc7607439fc039b849cace4f96532761f7bd7ba9
K4MIN4R <GhostPL1223@o2.pl>
2024-06-20 22:17:48 +0200

    Update Platform.cpp
    doxygen

--- Platform.cpp
+++ Platform.cpp
@@ -1,16 +1,29 @@
-//platform.cpp
 #include "Platform.h"
 #include <allegro5/allegro_primitives.h>
 
 const float Platform::width = 80;
 const float Platform::height = 20;
 
+/**
+ * @brief Konstruktor klasy Platform.
+ * 
+ * @param x Pozycja pozioma platformy.
+ * @param y Pozycja pionowa platformy.
+ */
 Platform::Platform(float x, float y) : x(x), y(y) {}
 
+/**
+ * @brief Przesuwa platformę w dół z zadaną prędkością.
+ * 
+ * @param speed Prędkość przesuwania platformy.
+ */
 void Platform::moveDown(float speed) {
     y += speed;
 }
 
+/**
+ * @brief Rysuje platformę na ekranie.
+ */
 void Platform::draw() const {
     al_draw_filled_rectangle(x - (width / 2), y - (height / 2), x + width + (width / 2), y + (height / 2), al_map_rgb(255, 255, 255));
 }
